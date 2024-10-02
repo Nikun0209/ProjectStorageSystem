56b4429f13e4ef222f23771a87140cfdd0f8329b
Nikun0209 <duonggiathanh3819@gmail.com>
2024-10-02 22:52:25 +0700

    commit

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
 import streamlit as st # type: ignore
+
+st.set_page_config(
+    page_title="BenCodeX",
+    page_icon="🤖",
+    layout="wide"
+)
+
 import home
 import login
 from streamlit_cookies_controller import CookieController # type: ignore
-
-# st.set_page_config(
-#     page_title="BenCodeX",
-#     page_icon="🤖",
-#     layout="wide"
-# )
 
 # Khởi tạo CookieController
 controller = CookieController()
