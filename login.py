87b1fd0a9910b459bd96730d4a99b89837f56fe6
Nikun0209 <duonggiathanh3819@gmail.com>
2024-10-04 17:01:53 +0700

    commit

--- login.py
+++ login.py
@@ -20,7 +20,7 @@ def login():
 
             if result or (username_input == "admin" and password_input == "123"):  # Thay thế bằng điều kiện kiểm tra thực tế
                 index.controller.set('username', username_input)
+                st.rerun()
                 home.home()
-                # st.rerun()
             else:
                 st.warning("Tên người dùng hoặc mật khẩu không đúng.")
